06153035336648e16858a4cbda60c1f31abfa291
Gurleen Kaur <gurleenkaurkhurpa19@gmail.com>
2024-07-27 20:16:40 -0400

    authentication added

--- public/script.js
+++ public/script.js
@@ -1,24 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
   const socket = io();
+  const container = document.getElementById("checkboxes-container");
 
+  // Listen for initial state from the server
   socket.on("initialState", (checkboxStates) => {
     checkboxStates.forEach((checked, index) => {
       document.getElementById(`checkbox-${index}`).checked = checked;
     });
   });
 
+  // Listen for updates from the server
   socket.on("checkboxUpdate", (data) => {
     document.getElementById(`checkbox-${data.index}`).checked = data.checked;
   });
 
-  const checkboxes = document.querySelectorAll(".checkbox");
-  checkboxes.forEach((checkbox, index) => {
-    checkbox.addEventListener("change", () => {
+  // Handle checkbox change events
+  container.addEventListener("change", (event) => {
+    if (event.target.classList.contains("checkbox")) {
+      const index = event.target.id.split("-")[1];
       const data = {
-        index: index,
-        checked: checkbox.checked,
+        index: parseInt(index),
+        checked: event.target.checked,
       };
       socket.emit("checkboxChange", data);
-    });
+    }
   });
 });
